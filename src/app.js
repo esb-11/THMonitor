0eb88bed4835a3d3b6d9d6ce32428cef5b278efd
eduardo <198397564+esb-11@users.noreply.github.com>
2025-07-19 18:05:30 -0300

    feat: create index router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express from "express";
+import indexRouter from "./routes/indexRouter.js";
 
 const app = express();
+app.use("/", indexRouter);
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`) );
