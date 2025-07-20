aaf4a8b05d9a0aba20447f95ff0f6ecc943be1fe
eduardo <198397564+esb-11@users.noreply.github.com>
2025-07-20 12:22:16 -0300

    change: import only functions that are used

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import "dotenv/config";
 import express from "express";
-import url from "url";
-import path from "path";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
 import indexRouter from "./routes/indexRouter.js";
 import dataRouter from "./routes/dataRouter.js";
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-app.set("views", path.join(__dirname, "views"));
+app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
