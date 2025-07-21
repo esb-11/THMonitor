7be7970f0e76fd66a96c3d81e3c066c214c25401
eduardo <198397564+esb-11@users.noreply.github.com>
2025-07-21 12:02:20 -0300

    feat: parse incoming data

--- src/controller/dataController.js
+++ src/controller/dataController.js
@@ -1,6 +1,22 @@
-function dataPost(req, res) {
-  console.log(req.body);
+function postData(req, res) {
+  const { sensor, temperature, humidity } = parseIncomingData(req.body);
   res.end();
 }
 
-export { dataPost };
+function parseIncomingData(data) {
+  if (
+    typeof data.id != "string" ||
+    typeof data.t_canal1 != "string" ||
+    typeof data.u_canal1 != "string"
+  ) {
+    throw new Error("Received data in incorrect format!");
+  }
+
+  const sensor = data.id;
+  const temperature = parseInt(data.t_canal1.replace(".", ""));
+  const humidity = parseInt(data.u_canal1.replace(".", ""));
+
+  return { sensor, temperature, humidity };
+}
+
+export { postData };
--- src/routes/dataRouter.js
+++ src/routes/dataRouter.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { dataPost } from "../controller/dataController.js";
+import { postData } from "../controller/dataController.js";
 
 const dataRouter = express.Router();
 dataRouter.use(express.json());
-dataRouter.post("/", dataPost);
+dataRouter.post("/", postData);
 
 export default dataRouter;
