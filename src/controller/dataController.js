9335aa9413f0186ec88ac434b24eb44097fe7723
eduardo <198397564+esb-11@users.noreply.github.com>
2025-07-21 12:55:51 -0300

    feat: check if humidity and temperature are correctly parsed

--- src/controller/dataController.js
+++ src/controller/dataController.js
@@ -8,13 +8,15 @@ function parseIncomingData(data) {
     typeof data.id != "string" ||
     typeof data.t_canal1 != "string" ||
     typeof data.u_canal1 != "string"
-  ) {
+  )
     throw new Error("Received data in incorrect format!");
-  }
 
   const sensor = data.id;
   const temperature = parseInt(data.t_canal1.replace(".", ""));
   const humidity = parseInt(data.u_canal1.replace(".", ""));
+
+  if (typeof humidity != "number" || typeof temperature != "number")
+    throw new Error("Received data in incorrect format!");
 
   return { sensor, temperature, humidity };
 }
