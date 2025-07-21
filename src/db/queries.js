cf0a7dfb4a33f04615f792e50052d7cdacd22f5b
eduardo <198397564+esb-11@users.noreply.github.com>
2025-07-21 13:22:39 -0300

    feat: add db queries

---
+++ src/db/queries.js
@@ -0,0 +1,31 @@
+import { query } from "./pool.js";
+
+async function getRecentData() {
+  const { rows } = await query("SELECT date, sensor, temperature, humidity, location, position FROM recent_data JOIN sensors ON recent_data.sensor_id = sensors.sensor_id JOIN locations ON recent_data.location_id = locations.location_id JOIN positions ON recent_data.position_id = positions.position_id");
+  return rows;
+}
+
+async function insertRecentData(data) {
+  const { sensor, temperature, humidity } = data;
+  
+  const sensor_id = await getSensorId(sensor);
+  const { location_id, position_id } = await getSensorPosition(sensor_id);
+  
+  await query("INSERT INTO recent_data (temperature, humidity, sensor_id, location_id, position_id) VALUES ($1, $2, $3, $4, $5)", [temperature, humidity, sensor_id, location_id, position_id]);
+}
+
+async function getSensorId(sensor) {
+  const { rows } = await query("SELECT sensor_id FROM sensors WHERE sensor = ($1)", [sensor]);
+  if (rows.length == 0) throw new Error("Sensor not found");
+  const sensor_id = rows[0].sensor_id;
+  return sensor_id;
+}
+
+async function getSensorPosition(sensor_id) {
+  const { rows } = await query("SELECT location_id, position_id FROM map WHERE sensor_id = ($1)", [sensor_id]);
+  if (rows.length == 0) throw new Error("Sensor not found");
+  const { location_id, position_id } = rows[0];
+  return { location_id, position_id };
+}
+
+export { getRecentData, insertRecentData };
