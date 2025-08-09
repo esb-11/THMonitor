0742c7bdb80c583b581feb5bc06e5339e39294c2
eduardo <198397564+esb-11@users.noreply.github.com>
2025-08-09 11:06:42 -0300

    fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function logData() {
   const url = "https://thmonitor-production.up.railway.app/sensors";
   const data = await fetch(url, {
     method: "GET",
-  }).then(response => response.json());
+  }).then((response) => response.json());
   console.log(data);
 }
 
