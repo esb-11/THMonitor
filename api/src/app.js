3a948fcc0a15d00cf9da684ea506595c982617f6
eduardo <198397564+esb-11@users.noreply.github.com>
2025-08-09 10:08:22 -0300

    feat: add CORS

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import routes from "./routes/index.js";
+import cors from "cors";
 import errorHandler from "./middleware/errorHandler.js";
 
+const corsOptions = {
+  origin: process.env.ORIGIN,
+};
+
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
